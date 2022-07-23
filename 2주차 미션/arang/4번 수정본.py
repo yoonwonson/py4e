51117c9a6f0f24fe6c62245b3ef7c1c8180bdbc2
Arraning <109465211+Arraning@users.noreply.github.com>
2022-07-24 01:57:22 +0900

    Update 4번 수정본.py

--- 2주차 미션/arang/4번 수정본.py
+++ 2주차 미션/arang/4번 수정본.py
@@ -9,17 +9,17 @@ method = str(input('결제수단은 무엇입니까? 카드 또는 현금으로 
 def busFare(age,method) :
     if age<8 or age>=75 :
         return '무료'
-    if age<14 :
+    elif age<14 :
         return '450원'
-    if age<20 :
+    elif age<20 :
         if method=='현금' :
             return '1000원'
-        if method=='카드' :
+        elif method=='카드' :
             return '720원'
-    if age<75 :
+    elif age<75 :
         if method=='현금' :
             return '1300원'
-        if method=='카드' :
+        elif method=='카드' :
             return '1200원'
             
 print('나이:',age)
