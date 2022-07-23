27e137a458bab9327c56d6292056928f932f97d7
yoonwonson <109283252+yoonwonson@users.noreply.github.com>
2022-07-23 22:56:54 +0900

    Update 4번.py

--- 2주차 미션/윤원/4번.py
+++ 2주차 미션/윤원/4번.py
@@ -4,31 +4,30 @@ pay = input("카드/현금 ")
 age = int(age)
 
 # 버스 요금을 알아내는 함수
-def bus_fare(age, payment):
+def busFare(age, payment):
     if payment == '카드':
         if age < 8 or age >= 75 :
            return '무료'
-        elif age < 14 or age >= 8 :
-            return '450원'
-        elif age < 20 or age >= 14 :
-            return '720원'
-        elif age < 75 or age>= 20 :
-
+        elif age >= 20 or age < 75 : 
             return '1200원'
+        elif age >= 14 or age < 20 :
+            return '720원'
+        elif age >= 8 or age < 14 :
+            return '450원'
         else :
             return '무료'
     else:
-        if age < 8 or age >= 75 :
+        if age < 8 or age >=75 :
                return '무료'
-        elif age < 14 or age >= 8 :
-            return '450원'
-        elif age < 20 or age >= 14 :
-            return '1000원'
-        elif age < 75 or age>= 20 :
+        elif age >= 20 or age < 75 : 
             return '1300원'
+        elif age >= 14 or age < 20 :
+            return '1000원'
+        elif age >= 8 or age < 14 :
+            return '450원'
         else :
             return '무료'
 
 print('나이:',age)
 print('지불유형:',pay)
-print('버스요금:',bus_fare(age, pay))
+print('버스요금:',busFare(age, pay))
