6d28ee8080c94251df25d5a4eed2131814886269
Arraning <109465211+Arraning@users.noreply.github.com>
2022-07-29 00:45:16 +0900

    Create 1번.py

---
+++ 3주차 미션/1번.py
@@ -0,0 +1,29 @@
+
+# 입력
+while True :
+    try :
+        number = int(input("몇 단? : "))
+        break
+    except :
+        print("숫자를 입력해주세요")
+        continue
+
+#구구단 함수
+def gugudan(number) :
+    zork = -1
+    value = 1 * number
+    while True :
+        if value <= 50 :
+            zork = zork + 2
+            value = number * zork
+            if value <= 50 :
+                 print(number,'X',zork,'=',value)
+            else :
+                break
+    print('끝!')
+
+
+# 출력
+print('몇 단? :',number)
+print(number,'단')
+print(gugudan(number))
