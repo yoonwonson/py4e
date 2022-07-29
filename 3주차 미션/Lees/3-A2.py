2c4968359071c0c38af516dd9a53291da52dba10
INIS-LEES\lsh <lsh@inissoft.com>
2022-07-29 17:55:25 +0900

    2게임

--- 3주차 미션/Lees/3-A2.py
+++ 3주차 미션/Lees/3-A2.py
@@ -1,9 +1,5 @@
 import random
 
-comCnt = 0
-userCnt = 0
-noneCnt = 0
-
 def calcRsp(rsp, computer) :
     global userCnt
     global noneCnt
@@ -41,26 +37,43 @@ def calcRsp(rsp, computer) :
     print("※※※※※※※※※※※※")
     print()
 
-games = int(input("몇 판을 진행하시겠습니까? : "))
+def playGame(games) :
+    for i in range(games) :
+        while (1) :
+            print("※※※※※※※",i+1,"번째 ※※※※※※※")
+            rsp = str(input("가위/바위/보를 입력하세요.(가위:0, 바위:1, 보:2) => "))
 
-for i in range(games) :
-    while (1) :
-        print("※※※※※※※",i+1,"번째 ※※※※※※※")
-        rsp = str(input("가위/바위/보를 입력하세요.(가위:0, 바위:1, 보:2) => "))
+            if (rsp == "0" or rsp == "1" or rsp == "2" or rsp == "가위" or rsp == "바위" or rsp == "보") :
+                computer = random.randint(0, 2)
+                calcRsp(rsp, computer)
+                break
+            else :
+                print()
+                print("※※※※※※※입력오류※※※※※※※")
+                print("잘못 입력하셨습니다.(입력값 => "+rsp+")")
+                print("※※※※※※※※※※※※※※※※※※")
+                print()
 
-        if (rsp == "0" or rsp == "1" or rsp == "2" or rsp == "가위" or rsp == "바위" or rsp == "보") :
-            computer = random.randint(0, 2)
-            calcRsp(rsp, computer)
-            break
-        else :
-            print()
-            print("※※※※※※※입력오류※※※※※※※")
-            print("잘못 입력하셨습니다.(입력값 => "+rsp+")")
-            print("※※※※※※※※※※※※※※※※※※")
-            print()
+    print()
+    print("※※※※ 총 결과 ※※※※")
+    print("▶사용자 : 승", userCnt, "무", noneCnt, "패", comCnt)
+    print("▶컴퓨터 : 승", comCnt, "무", noneCnt, "패", userCnt)
+    print("※※※※※※※※※※※※※")
 
-print()
-print("※※※※가위바위보※※※※")
-print("▶사용자 : 승", userCnt, "무", noneCnt, "패", comCnt)
-print("▶컴퓨터 : 승", comCnt, "무", noneCnt, "패", userCnt)
-print("※※※※※※※※※※※※")
+
+comCnt = 0
+userCnt = 0
+noneCnt = 0
+
+while(1) :
+    games = input("몇 판을 진행하시겠습니까? : ")
+    try:
+         games = int(games)
+         playGame(games)
+         break
+    except Exception as e:
+        print()
+        print("※※※※※※※입력오류※※※※※※※")
+        print("숫자를 입력해주세요.(입력값 => "+games+")")
+        print("※※※※※※※※※※※※※※※※※※")
+        print()
