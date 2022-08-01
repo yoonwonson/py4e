26eac538b9a54eb5c38751d9d0aaaa3fd877649c
Arraning <109465211+Arraning@users.noreply.github.com>
2022-08-02 03:11:35 +0900

    Update 3번.py

--- 3주차 미션/arang/3번.py
+++ 3주차 미션/arang/3번.py
@@ -1,9 +1,3 @@
-
-# 입력
-n = int(input("첫 번째 수 입력 : "))
-m = int(input("두 번째 수 입력 : "))
-numbers = [ i for i in range(n,m+1)]
-
 # 짝수, 중앙값
 def find_even_number(n, m):
     for thing in numbers :
@@ -11,5 +5,26 @@ def find_even_number(n, m):
             print(thing,'짝수')
     if ((n+m)/2)%2 == 0 :
         print('중앙값 :',int(((n+m)/2)))
+
+# 입력
+while True :
+    try :
+        n = int(input("첫 번째 수 입력 : "))
+        break
+    except :
+        print('숫자를 입력해주세요')
+        continue
+while True :
+    try :
+        m = int(input("두 번째 수 입력 : "))
+        if m<= n :
+            (print('첫 번째 수보다 큰 숫자를 입력해주세요.'))
+        else :
+            break
+    except :
+        print('숫자를 입력해주세요')
+        continue
+numbers = [ i for i in range(n,m+1)]
+
 # 출력
 find_even_number(n,m)
